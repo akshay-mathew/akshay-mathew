421e5f5ab8fc1b16fc1c52aeeff3cee4052fd80e
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2024-04-03 11:04:31 -0400

    Update main.js

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -1,3 +1,8 @@
+// Name: Akshay Mathew
+// File: main.js
+// Date: 03 April, 2024
+// Description: Creating an image gallery and getting more in depth idea about javascript. 
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,7 +10,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
