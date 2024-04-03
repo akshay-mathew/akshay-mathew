cb9c942a32b602d7687d3e37b78ba570c2c515b3
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2024-04-03 10:50:21 -0400

    Update main.js

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -29,4 +29,29 @@ function result() {
     const yItem = randomValueFromArray(insertY);
     const zItem = randomValueFromArray(insertZ);
 
+    newStory = newStory
+                .replaceAll(":insertx:", xItem)
+                .replace(":inserty:", yItem)
+                .replace(":insertz:", zItem);
+ 
+
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature =  `${Math.round((94 - 32) * (5/9))} centigrade`;
+    newStory = newStory
+                    .replace("300 pounds", weight)
+                    .replace("94 fahrenheit", temperature)
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
     
