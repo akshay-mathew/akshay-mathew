122d513ecdaec3a74c82067d1e2df92e100399a3
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2024-04-03 12:14:52 -0400

    Update main-finished.js

--- Assignment 4/Part 4/main-finished.js
+++ Assignment 4/Part 4/main-finished.js
@@ -144,7 +144,22 @@ class EvilCircle extends Shape {
       this.y += this.size;
     }
   }
-
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+          countDisplay.textContent = 'Ball count: ' + count;
+        }
+      }
+    }
+  }
+}
 
 
 
