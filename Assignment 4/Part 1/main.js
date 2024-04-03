9134c4bd114b39ceaac302f96d38859c0c0c9bff
Akshay Mathew <156467235+akshay-mathew@users.noreply.github.com>
2024-04-03 10:43:15 -0400

    Update main.js

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -3,3 +3,11 @@
 // Date: 04 April, 2024
 // Description: Creating a stroy generator using html and javascript. The different aspects of java
 //              script are used in this.
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
